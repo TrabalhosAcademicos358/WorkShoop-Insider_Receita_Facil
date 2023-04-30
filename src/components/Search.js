68382d09f70846c6d59dab8c6d785bee009bbf73
Pedro Júnior <pedrojuniorifpb@gmail.com>
2023-04-30 11:16:52 -0300

    Pagina de Search criada

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,25 @@
 import { useState } from 'react';
 import { View, TextInput, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
 
-import { styles } from '../styles.js'
+import { styles } from '../styles'
+import { namePages } from "../routes/namePages"
 
 export default function Search() {
     const [value, setValue] = useState("");
+    const navigation = useNavigation();
+
+    function handleSearch() {
+        if (value.trim() === "") return null;
+        const textInput = value;
+        setValue("");
+        navigation.navigate(
+            namePages.search, 
+            { text: textInput }
+        );
+    }
+
     return (
         <View style={styles.viewSearch}>
             <TextInput
@@ -15,7 +29,7 @@ export default function Search() {
                 placeholderTextColor="#AAA"
                 value={value}
             />
-            <TouchableOpacity onPress={() => console.log(value)}>
+            <TouchableOpacity onPress={handleSearch}>
                 <Ionicons name="search" color="#46BD6A" size={24} />
             </TouchableOpacity>
         </View>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,42 @@
-import { SafeAreaView } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { SafeAreaView, Text } from "react-native";
+import { useEffect, useState } from "react";
+
 import { styles } from "../styles";
+import { getFavorites } from "../services/favorites"
 
-import ListFood from "../components/ListFood.js";
-
-import db from '../../db.json'
+import ListFood from "../components/ListFood";
+import db from '../../db.json';
 
 export default function Search() {
+    const route = useRoute()
+    const { text } = route.params
+    const [foods, setFoods] = useState([])
+
+    useEffect(() => {
+        async function updateFoods() {
+            const listFoods = db.foods;
+            const listFilter = listFoods.filter(item => {
+                const name = item.name;
+                return name.includes(text);
+            });
+            setFoods(listFilter);
+        }
+
+        updateFoods()
+    }, [text])
+
     return (
-        <SafeAreaView style={styles.page}>
-            <ListFood list={db.foods} />
+        <SafeAreaView style={[styles.page, { paddingTop: 40 }]}>
+            {
+                foods.length === 0 ? (
+                    <Text style={{ fontSize: 16 }}>
+                        Ops não encontramos sua receita...
+                    </Text>
+                ) : (
+                    <ListFood list={foods} />
+                )
+            }
         </SafeAreaView>
     )
 }
